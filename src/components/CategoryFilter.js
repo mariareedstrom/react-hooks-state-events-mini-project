5b6c6638143cffd07eefa0b8936bfe9bd91ff43c
Maria Reedstrom <maria.reedstrom@gmail.com>
2022-02-01 09:06:14 -0600

    commit tree 1d37dda8b6d76014182206624a4c83b2fd295458

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,34 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({
+  selectedCategory,
+  categories,
+  filterTasksByCategory,
+}) {
+  function renderButton() {
+    return categories.map((category) => {
+      const className = selectedCategory === category ? "selected" : "";
+      return (
+        <button
+          key={category}
+          onClick={handleButtonClick}
+          className={className}
+        >
+          {category}
+        </button>
+      );
+    });
+  }
+
+  function handleButtonClick(e) {
+    const category = e.target.textContent;
+    filterTasksByCategory(category);
+  }
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {renderButton()}
     </div>
   );
 }
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,19 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [details, setDetails] = useState("");
+  const [category, setCategory] = useState();
+
+  function renderCategories() {
+    return categories
+      .filter((category) => category !== "All")
+      .map((category) => {
+        return (
+          <option key={category} value={category}>
+            {category}
+          </option>
+        );
+      });
+  }
+
+  function handleChange(e) {
+    if (e.target.name === "text") {
+      setDetails(e.target.value);
+    } else if (e.target.name === "category") {
+      setCategory(e.target.value);
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    onTaskFormSubmit({ text: details, category });
+    setDetails("");
+  }
+
   return (
     <form className="new-task-form">
       <label>
         Details
-        <input type="text" name="text" />
+        <input
+          type="text"
+          name="text"
+          onChange={handleChange}
+          id="task-details"
+        />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" onChange={handleChange}>
+          {renderCategories()}
         </select>
       </label>
-      <input type="submit" value="Add task" />
+      <input
+        type="submit"
+        value="Add task"
+        onSubmit={handleSubmit}
+        id="task-category"
+      />
     </form>
   );
 }
